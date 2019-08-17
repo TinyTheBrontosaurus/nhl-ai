391a4a53f66b2233f6da0aa9d724d2006910b3c5
Eric Nelson <big89e@gmail.com>
2019-08-17 18:17:46 -0400

    Create a utils class -- not tested

--- training/info_utils.py
+++ training/info_utils.py
@@ -1,39 +1,56 @@
 
-def get_player_w_puck(info):
-    player_w_puck = {}
 
-    for team in ('home', 'away'):
-        for position in ('LW', 'C', 'RW', 'LD', 'RD', 'G'):
-            possible_match = True
-            for dim in ('x', 'y'):
-                player_label = 'player-{}-{}-{}'.format(team, position, dim)
-                puck_pos = 'player-w-puck-ice-{}'.format(dim)
-                if info[player_label] != info[puck_pos]:
-                    possible_match = False
+class InfoWrapper:
 
-            if possible_match:
-                player_w_puck['team'] = team
-                player_w_puck['pos'] = position
-                return player_w_puck
+    def __init__(self):
+        # Info that comes from env
+        self.info: dict = {}
 
-    return {}
+    @property
+    def player_w_puck(self):
+        """
+        Returns info about the play with the puck, including 'team' and 'position'.
+        Empty dict if no possessor
+        :return:
+        """
 
+        player_w_puck = {}
 
-def players_and_puck_feature(info):
-    features = []
+        for team in ('home', 'away'):
+            for position in ('LW', 'C', 'RW', 'LD', 'RD', 'G'):
+                possible_match = True
+                for dim in ('x', 'y'):
+                    player_label = 'player-{}-{}-{}'.format(team, position, dim)
+                    puck_pos = 'player-w-puck-ice-{}'.format(dim)
+                    if self.info[player_label] != self.info[puck_pos]:
+                        possible_match = False
 
-    for team in ('home', 'away'):
-        for position in ('LW', 'C', 'RW', 'LD', 'RD', 'G'):
-            for dim in ('x', 'y'):
-                player_label = 'player-{}-{}-{}'.format(team, position, dim)
-                features.append(info[player_label])
+                if possible_match:
+                    player_w_puck['team'] = team
+                    player_w_puck['pos'] = position
+                    return player_w_puck
 
-    for dim in ('x', 'y'):
-        features.append(info['player-w-puck-ice-{}'.format(dim)])
+        return {}
 
-    for dim in ('x', 'y'):
-        features.append(info['puck-ice-{}'.format(dim)])
+    @property
+    def players_and_puck_feature(self):
+        """
+        :return: All of the player positions and puck position in a list. Useful as a feature vector
+        """
+        features = []
 
-    features.append(0 if get_player_w_puck(info) is {} else 1)
+        for team in ('home', 'away'):
+            for position in ('LW', 'C', 'RW', 'LD', 'RD', 'G'):
+                for dim in ('x', 'y'):
+                    player_label = 'player-{}-{}-{}'.format(team, position, dim)
+                    features.append(self.info[player_label])
 
-    return features
+        for dim in ('x', 'y'):
+            features.append(self.info['player-w-puck-ice-{}'.format(dim)])
+
+        for dim in ('x', 'y'):
+            features.append(self.info['puck-ice-{}'.format(dim)])
+
+        features.append(0 if self.player_w_puck is {} else 1)
+
+        return features
