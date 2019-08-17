f84c4d2ad36df1526d19d03274987c98c7b6d10e
Eric Nelson <big89e@gmail.com>
2019-08-17 19:42:21 -0400

    Testing of player w puck

--- training/test/test_info_utils.py
+++ training/test/test_info_utils.py
@@ -1,29 +1,53 @@
 
-
+import pytest
 from training.info_utils import InfoWrapper, InfoAccumulator
 
+TEAMS = ('home', 'away')
+POSITIONS = ('LW', 'C', 'RW', 'LD', 'RD', 'G')
+DIMS = ('x', 'y')
 
-def test_player_w_puck():
+@pytest.fixture(name='info')
+def _info():
+    info = {}
+    for team in TEAMS:
+        for position in POSITIONS:
+            for dim in DIMS:
+                info['player-{}-{}-{}'.format(team, position, dim)] = 0
+
+    for dim in DIMS:
+        info['player-w-puck-ice-{}'.format(dim)] = 0
+
+    return info
+
+@pytest.mark.parametrize('team,pos', [(team, pos) for team in TEAMS for pos in POSITIONS])
+def test_player_has_puck(info, team, pos):
     # Arrange
     object_under_test = InfoWrapper()
+    object_under_test.info = info
 
-    for team in ('home', 'away'):
-        for position in ('LW', 'C', 'RW', 'LD', 'RD', 'G'):
-            for dim in ('x', 'y'):
-                object_under_test.info['player-{}-{}-{}'.format(team, position, dim)] = 0
-
-    for dim in ('x', 'y'):
-        object_under_test.info['player-w-puck-ice-{}'.format(dim)] = 0
-
-    object_under_test.info['player-home-LW-x'] = 100
-    object_under_test.info['player-home-LW-y'] = 200
-    object_under_test.info['player-w-puck-ice-x'] = 100
-    object_under_test.info['player-w-puck-ice-y'] = 200
+    info['player-{}-{}-x'.format(team, pos)] = 100
+    info['player-{}-{}-y'.format(team, pos)] = 200
+    info['player-w-puck-ice-x'] = 100
+    info['player-w-puck-ice-y'] = 200
 
     # Act
     actual = object_under_test.player_w_puck
 
     # Assert
-    assert actual == {'team': 'home', 'pos': 'LW'}
+    assert actual == {'team': team, 'pos': pos}
 
+
+def test_no_possession(info):
+    # Arrange
+    object_under_test = InfoWrapper()
+    object_under_test.info = info
+
+    info['player-w-puck-ice-x'] = 100
+    info['player-w-puck-ice-y'] = 200
+
+    # Act
+    actual = object_under_test.player_w_puck
+
+    # Assert
+    assert actual == {}
 
