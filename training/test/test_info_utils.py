5a7e00f08def91c0c1a6c6ac0ac632d063aba733
Eric Nelson <big89e@gmail.com>
2019-08-17 21:22:48 -0400

    Using pytest-steps

--- training/test/test_info_utils.py
+++ training/test/test_info_utils.py
@@ -1,15 +1,18 @@
 
 import pytest
+import pytest_steps
 from training.info_utils import InfoWrapper, InfoAccumulator, TEAMS, POSITIONS, DIMS, TIME_PER_FRAME
 
 
 @pytest.fixture(name='info')
 def _info():
     info = {}
-    for team in TEAMS:
-        for position in POSITIONS:
-            for dim in DIMS:
-                info['player-{}-{}-{}'.format(team, position, dim)] = 0
+    for teami, team in enumerate(TEAMS):
+        for positioni, position in enumerate(POSITIONS):
+            for dimi, dim in enumerate(DIMS):
+                info['player-{}-{}-{}'.format(team, position, dim)] = (teami + 1) * 100 + \
+                                                                      (positioni + 1) * 10 + \
+                                                                      dimi + 1
 
     for dim in DIMS:
         info['player-w-puck-ice-{}'.format(dim)] = 0
@@ -78,12 +81,20 @@ def test_feature(info):
     assert actual == expected
 
 
-def test_accumulator_default(info):
+@pytest_steps.test_steps('ctor', 'first_possession', 'first_pass_direct', 'held', 'in_transit', 'received')
+def test_accumulator(info):
+
+    def move_puck_to(team, pos):
+        info['player-w-puck-ice-x'] = info['player-{}-{}-x'.format(team, pos)]
+        info['player-w-puck-ice-y'] = info['player-{}-{}-y'.format(team, pos)]
+
+    def clear_puck():
+        info['player-w-puck-ice-x'] = 0
+        info['player-w-puck-ice-y'] = 0
+
     # Arrange
     object_under_test = InfoAccumulator()
     object_under_test.info = info
-    info['player-w-puck-ice-x'] = 100
-    info['player-w-puck-ice-y'] = 200
 
     # Act
     object_under_test.accumulate()
@@ -98,19 +109,11 @@ def test_accumulator_default(info):
                                            'away': pytest.approx(0.0)}
     assert object_under_test.consecutive_passes == {'consecutive': {'home': [0], 'away': [0]},
                                                     'unique':  {'home': [0], 'away': [0]},}
+    yield
 
-def test_accumulator_first_poss(info):
+
     # Arrange
-    object_under_test = InfoAccumulator()
-    object_under_test.info = info
-
-    # Previous test
-    info['player-w-puck-ice-x'] = 100
-    info['player-w-puck-ice-y'] = 200
-    object_under_test.accumulate()
-
-    info['player-{}-{}-x'.format('home', 'LW')] = 100
-    info['player-{}-{}-y'.format('home', 'LW')] = 200
+    move_puck_to('home', 'LW')
 
     # Act
     object_under_test.accumulate()
@@ -125,24 +128,10 @@ def test_accumulator_first_poss(info):
                                            'away': pytest.approx(0.0)}
     assert object_under_test.consecutive_passes == {'consecutive': {'home': [0], 'away': [0]},
                                                     'unique':  {'home': [0], 'away': [0]},}
+    yield
 
-def test_accumulator_first_pass_direct(info):
     # Arrange
-    object_under_test = InfoAccumulator()
-    object_under_test.info = info
-
-    # Previous test
-    info['player-w-puck-ice-x'] = 100
-    info['player-w-puck-ice-y'] = 200
-    object_under_test.accumulate()
-    info['player-{}-{}-x'.format('home', 'LW')] = 100
-    info['player-{}-{}-y'.format('home', 'LW')] = 200
-    object_under_test.accumulate()
-
-    info['player-w-puck-ice-x'] = 150
-    info['player-w-puck-ice-y'] = 250
-    info['player-{}-{}-x'.format('home', 'RW')] = 150
-    info['player-{}-{}-y'.format('home', 'RW')] = 250
+    move_puck_to('home', 'RW')
 
     # Act
     object_under_test.accumulate()
@@ -157,5 +146,60 @@ def test_accumulator_first_pass_direct(info):
                                            'away': pytest.approx(0.0)}
     assert object_under_test.consecutive_passes == {'consecutive': {'home': [1], 'away': [0]},
                                                     'unique':  {'home': [1], 'away': [0]},}
+    yield
 
+    # Arrange
+
+    # Act
+    object_under_test.accumulate()
+
+    # Assert
+    assert object_under_test.pass_attempts == {'home': 1, 'away': 0}
+    assert object_under_test.pass_count == {'home': 1, 'away': 0}
+    assert object_under_test.steal_count == {'home': 0, 'away': 0}
+
+    assert object_under_test.time_puck == {'home': pytest.approx(TIME_PER_FRAME * 3),
+                                           None: pytest.approx(TIME_PER_FRAME * 1),
+                                           'away': pytest.approx(0.0)}
+    assert object_under_test.consecutive_passes == {'consecutive': {'home': [1], 'away': [0]},
+                                                    'unique': {'home': [1], 'away': [0]}, }
+    yield
+
+    # In transit
+    # Arrange
+    clear_puck()
+
+    # Act
+    object_under_test.accumulate()
+
+    # Assert
+    assert object_under_test.pass_attempts == {'home': 2, 'away': 0}
+    assert object_under_test.pass_count == {'home': 1, 'away': 0}
+    assert object_under_test.steal_count == {'home': 0, 'away': 0}
+
+    assert object_under_test.time_puck == {'home': pytest.approx(TIME_PER_FRAME * 3),
+                                           None: pytest.approx(TIME_PER_FRAME * 2),
+                                           'away': pytest.approx(0.0)}
+    assert object_under_test.consecutive_passes == {'consecutive': {'home': [1], 'away': [0]},
+                                                    'unique': {'home': [1], 'away': [0]}, }
+    yield
+
+    # Received
+    # Arrange
+    move_puck_to('home', 'LW')
+
+    # Act
+    object_under_test.accumulate()
+
+    # Assert
+    assert object_under_test.pass_attempts == {'home': 2, 'away': 0}
+    assert object_under_test.pass_count == {'home': 2, 'away': 0}
+    assert object_under_test.steal_count == {'home': 0, 'away': 0}
+
+    assert object_under_test.time_puck == {'home': pytest.approx(TIME_PER_FRAME * 4),
+                                           None: pytest.approx(TIME_PER_FRAME * 2),
+                                           'away': pytest.approx(0.0)}
+    assert object_under_test.consecutive_passes == {'consecutive': {'home': [2], 'away': [0]},
+                                                    'unique': {'home': [1], 'away': [0]}, }
+    yield
 
