1a33a4755bcd8a6094f3a04c033bde698cf75be4
Eric Nelson <big89e@gmail.com>
2020-01-11 15:55:59 -0500

    Add comments to config

--- crosscheck/__main__.py
+++ crosscheck/__main__.py
@@ -6,27 +6,43 @@ from loguru import logger
 
 
 template = {
-    'mode': confuse.OneOf(['train', 'replay', 'compete']),
+    # The mode in which to run
+    #  'train': Create a new model
+    #  'replay': Run an existing model
+    #  'compete': Run two models against each other
+    #  'play': Run a model against a human player (controller input)
+    #  'play-2p': Play traditional human vs human
+    'mode': confuse.OneOf(['train', 'replay', 'compete', 'play', 'play-2p']),
     'input': {
+        # The 1+ scenarios that are run in parallel
         'scenarios': confuse.Sequence({
             'scenario': str,
             'scorekeeper': str,
         }),
+        # The cost function to use when combining scenarios
         'metascorekeeper': str,
     },
+    # Information about the movie to record
     'movie': {
-        'enabled': confuse.OneOf(['all', 'generations', 'latest', 'off']),
+        # When to create a movie
+        # 'generations': The best of each generation (unless duplicated)
+        # 'latest': Only the best of the latest generation
+        # 'off': No movie
+        'enabled': confuse.OneOf(['generations', 'latest', 'off']),
+        # How long to keep playing the movie after the scenario says it is done
         'stoppage-time-s': float,
     },
+    # True to show the movie in a window during execution
     'render-live': bool,
+    # The number of processes to run
     'nproc': int,
 }
 
-config = confuse.LazyConfig('Cross-check: NHL \'94 reinforcement learning', __name__)
+config = confuse.LazyConfig('cross-check', __name__)
 
 
 def main(argv):
-    parser = argparse.ArgumentParser(description='Cross-check')
+    parser = argparse.ArgumentParser(description='Cross-check: NHL \'94 reinforcement learning')
     parser.add_argument('--config', '-c', help='App config.')
     parser.add_argument('--nproc', type=int,
                         dest="nproc",
@@ -43,12 +59,14 @@ def main(argv):
         config.set_file(args.config)
     config.set_args(args, dots=True)
 
-    logger.debug('configuration directory is', config.config_dir())
+    logger.debug('configuration directory is {}', config.config_dir())
 
     if config['nproc'].get() is None:
         config['nproc'] = multiprocessing.cpu_count()
 
     valid = config.get(template)
+
+    pass
 
 
 if __name__ == "__main__":
