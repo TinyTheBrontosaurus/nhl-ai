2f25891bb4542f7670613001d2a8a3f19e2cc2fb
Eric Nelson <big89e@gmail.com>
2020-01-19 14:32:56 -0500

    Add date folder to logger

--- crosscheck/log_folder.py
+++ crosscheck/log_folder.py
@@ -5,13 +5,15 @@ class LogFolder:
     folder = pathlib.Path.cwd()
     friendly_time = None
     friendly_name = None
+    start_time = datetime.datetime.now()
 
     @classmethod
     def set_path(cls, root: pathlib.Path, friendly_name: str):
         # Setup target log folder
+        the_date = cls.start_time.date()
         cls.friendly_time = str(datetime.datetime.now()).replace(':', "-").replace(" ", "_")
         cls.friendly_name = friendly_name
-        cls.folder = pathlib.Path(root) / cls.friendly_name / cls.friendly_time
+        cls.folder = pathlib.Path(root) / cls.friendly_name / str(the_date) / cls.friendly_time
 
         # Create the folders
         cls.folder.mkdir(parents=True, exist_ok=True)
