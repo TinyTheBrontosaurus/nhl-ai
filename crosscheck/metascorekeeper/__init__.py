5eb3c27945957617dfff5e485d4366cbbd52242d
Eric Nelson <big89e@gmail.com>
2020-01-26 06:06:42 -0500

    Add nudged median

--- crosscheck/metascorekeeper/__init__.py
+++ crosscheck/metascorekeeper/__init__.py
@@ -1,6 +1,7 @@
 from . import summer
 from . import mean
 from . import median
+from . import nudged_median
 from.base import Metascorekeeper
 
 # Hash to convert a string to a class ctor
@@ -8,4 +9,5 @@ string_to_class = {
     'summer': summer.Summer,
     'mean': mean.Mean,
     'median': median.Median,
+    'nudged-median': nudged_median.NudgedMedian
 }
---
+++ crosscheck/metascorekeeper/nudged_median.py
@@ -0,0 +1,40 @@
+from typing import List
+from ..scorekeeper import Scorekeeper
+from .base import Metascorekeeper
+from .mean import Mean
+from .median import Median
+
+class NudgedMedian(Metascorekeeper):
+
+    def __init__(self):
+        super().__init__()
+        self._mean = Mean()
+        self._median = Median()
+
+    def add(self, name: str, scorekeeper: Scorekeeper):
+        """
+        Add a scorekeeper
+        :param name: Name of the keeper
+        :param scorekeeper: The keeper
+        """
+        super().add(name, scorekeeper)
+        self._mean.add(name, scorekeeper)
+        self._median.add(name, scorekeeper)
+
+    @property
+    def score(self):
+        lowest = min([x.score for x in self._scorekeepers])
+        return self._median.score + self._mean.score * 0.01 + lowest
+
+    @classmethod
+    def fitness_threshold(cls, scorekeepers: List[Scorekeeper]) -> float:
+        return Median.fitness_threshold(scorekeepers)
+
+    def stats(self):
+        return self._median.stats()
+
+    def score_listing(self) -> List[float]:
+        """
+        Sorted for easier quick viewing of median
+        """
+        return self._median.score_listing()
