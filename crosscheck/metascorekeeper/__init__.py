d1acf4bf5c36649cc89a0434f6ab8dfbd97ae5d7
Eric Nelson <big89e@gmail.com>
2020-01-26 04:21:18 -0500

    Add median metascorekeeper

--- crosscheck/metascorekeeper/__init__.py
+++ crosscheck/metascorekeeper/__init__.py
@@ -1,9 +1,11 @@
 from . import summer
 from . import mean
+from . import median
 from.base import Metascorekeeper
 
 # Hash to convert a string to a class ctor
 string_to_class = {
     'summer': summer.Summer,
     'mean': mean.Mean,
+    'median': median.Median,
 }
---
+++ crosscheck/metascorekeeper/median.py
@@ -0,0 +1,24 @@
+from typing import List
+from ..scorekeeper import Scorekeeper
+from .base import Metascorekeeper
+import numpy as np
+
+
+class Median(Metascorekeeper):
+
+    def __init__(self):
+        super().__init__()
+
+    @property
+    def score(self):
+        return np.median(np.array([x.score for x in self._scorekeepers.values()]))
+
+    @classmethod
+    def fitness_threshold(cls, scorekeepers: List[Scorekeeper]) -> float:
+        return sum([x.fitness_threshold() for x in scorekeepers]) / len(scorekeepers)
+
+    def stats(self):
+        stats = {}
+        for sk in self._scorekeepers:
+            stats.update(sk.stats)
+        return stats
