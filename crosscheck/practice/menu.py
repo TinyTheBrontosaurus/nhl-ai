7bf65efd02012d01be3b5cd7a8aa1d2a3cc3d063
Eric Nelson <big89e@gmail.com>
2020-02-17 22:08:02 -0500

    Third level menu--very flaky

--- crosscheck/practice/menu.py
+++ crosscheck/practice/menu.py
@@ -3,6 +3,8 @@ import numpy as np
 from typing import List
 from crosscheck.player.utils import RisingEdge
 from PIL import Image, ImageDraw
+import yaml
+import crosscheck.definitions
 
 
 class Display:
@@ -72,8 +74,8 @@ class Menu(Display):
         return self._next_menu
 
 
-class TopMenu(Menu):
-    def __init__(self):
+class BasicMenu(Menu):
+    def __init__(self, options: dict):
         super().__init__()
         self.selection = 0
         self.up = RisingEdge()
@@ -81,6 +83,7 @@ class TopMenu(Menu):
         self.select = RisingEdge()
         self.back = RisingEdge()
         self._lines = []
+        self.options = options
 
     def tick(self, buttons: dict):
 
@@ -89,26 +92,54 @@ class TopMenu(Menu):
         self.select.update(buttons.get("A") or buttons.get("C") or buttons.get("START"))
         self.back.update(buttons.get("B"))
 
-        options = ["Play minigame", "Quit"]
 
         if self.up.state:
             self.selection -= 1
             if self.selection < 0:
-                self.selection = len(options) - 1
+                self.selection = len(self.options) - 1
         if self.down.state:
             self.selection += 1
-            if self.selection >= len(options):
+            if self.selection >= len(self.options):
                 self.selection = 0
 
         self._lines = [f"{'> ' if idx == self.selection else '':2} {label}"
-                       for idx, label in enumerate(options)]
+                       for idx, label in enumerate(self.options.keys())]
 
         if self.select.state:
-            if 1 == self.selection:
+            next_menus = list(self.options.values())
+            if self.selection >= len(next_menus):
                 self._next_menu = None
+            else:
+                next_menu_type = next_menus[self.selection]
+                if next_menu_type is None:
+                    self._next_menu = next_menu_type
+                else:
+                    self._next_menu = next_menu_type()
         if self.back.state:
             self._next_menu = None
 
     @property
     def lines(self):
         return self._lines
+
+
+class TopMenu(BasicMenu):
+    def __init__(self):
+        super().__init__({
+            "Play minigame": MinigameMenu,
+            "Quit": None
+        })
+
+
+class MinigameMenu(BasicMenu):
+
+    def __init__(self):
+        folder = crosscheck.definitions.SAVE_STATE_FOLDER / "02-2p"
+        manifest = folder / "manifest.yml"
+
+        with manifest.open() as f:
+            self._manifest = yaml.safe_load(f)
+
+        options = {f"{x['name']} ({x['file']})": None for x in self._manifest}
+        super().__init__(options)
+
